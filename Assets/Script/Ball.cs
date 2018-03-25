06871921155950f47090196a335458aaca223b60
dztientertainment <34145191+dztientertainment@users.noreply.github.com>
2018-03-25 18:54:00 -0300

    Add files via upload

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	//Controla o bola do jogo
 	void Update () {
 		if (GetComponent<Rigidbody2D> ().velocity != Vector2.zero)
 			GetComponent<Rigidbody2D> ().velocity = Vector2.Lerp (GetComponent<Rigidbody2D> ().velocity, Vector2.zero,Field.adhesion*0.005f);
